e13696c70a820f4150a49bc6044075728e56ee00
CodeAnt AI <chinmay@codeant.ai>
2023-12-17 16:19:54 +0000

    Changed files

--- dataset/cifar.py
+++ dataset/cifar.py
@@ -40,7 +40,7 @@ class CIFAR100(Dataset):
         self.std = [0.2675, 0.2565, 0.2761]
         self.normalize = transforms.Normalize(mean=self.mean, std=self.std)
         self.pretrain = pretrain
-
+    
         if transform is None:
             if self.partition == 'train' and self.data_aug:
                 self.transform = transforms.Compose([
@@ -60,14 +60,22 @@ class CIFAR100(Dataset):
                 ])
         else:
             self.transform = transform
-
-
+    
+    
         self.file_pattern = '%s.pickle'
         if not self.pretrain:
             pass
-
+    
         self.data = {}
-
+    
+        load_and_preprocess_data,(self, partition)
+    
+        # pre-process for contrastive sampling
+        self.k = k
+        self.is_sample = is_sample
+        preprocess_contrastive_sampling(self)
+    
+    def load_and_preprocess_data,(self, partition):
         with open(os.path.join(self.data_root, self.file_pattern % partition), 'rb') as f:
             data = pickle.load(f, encoding='latin1')
             self.imgs = data['data']
@@ -81,30 +89,28 @@ class CIFAR100(Dataset):
                     cur_class += 1
             new_labels = [label2label[label] for (idx, label) in enumerate(labels)]
             self.labels = new_labels
-
-        # pre-process for contrastive sampling
-        self.k = k
-        self.is_sample = is_sample
+    def preprocess_contrastive_sampling(self):
         if self.is_sample:
             self.labels = np.asarray(self.labels)
             self.labels = self.labels - np.min(self.labels)
             num_classes = np.max(self.labels) + 1
-
+    
             self.cls_positive = [[] for _ in range(num_classes)]
             for i in range(len(self.imgs)):
                 self.cls_positive[self.labels[i]].append(i)
-
+    
             self.cls_negative = [[] for _ in range(num_classes)]
             for i in range(num_classes):
                 for j in range(num_classes):
                     if j == i:
                         continue
                     self.cls_negative[i].extend(self.cls_positive[j])
-
+    
             self.cls_positive = [np.asarray(self.cls_positive[i]) for i in range(num_classes)]
             self.cls_negative = [np.asarray(self.cls_negative[i]) for i in range(num_classes)]
             self.cls_positive = np.asarray(self.cls_positive)
             self.cls_negative = np.asarray(self.cls_negative)
+    
 
     def __getitem__(self, item):
         """
@@ -175,6 +181,13 @@ class MetaCIFAR100(CIFAR100):
         self.classes = list(self.data.keys())
         self.n_test_runs = args.n_test_runs
         self.n_aug_support_samples = args.n_aug_support_samples
+        set_train_transform(train_transform, self)
+    
+        set_test_transform(test_transform, self)
+    
+        assign_images_to_classes(self)
+    
+    def set_train_transform(train_transform, self):
         if train_transform is None:
             self.train_transform = transforms.Compose([
                 lambda x: Image.fromarray(x),
@@ -187,7 +200,8 @@ class MetaCIFAR100(CIFAR100):
             ])
         else:
             self.train_transform = train_transform
-
+    
+    def set_test_transform(test_transform, self):
         if test_transform is None:
             self.test_transform = transforms.Compose([
                 lambda x: Image.fromarray(x),
@@ -196,13 +210,14 @@ class MetaCIFAR100(CIFAR100):
             ])
         else:
             self.test_transform = test_transform
-
+    def assign_images_to_classes(self):
         self.data = {}
         for idx in range(self.imgs.shape[0]):
             if self.labels[idx] not in self.data:
                 self.data[self.labels[idx]] = []
             self.data[self.labels[idx]].append(self.imgs[idx])
         self.classes = list(self.data.keys())
+    
 
     def __getitem__(self, item):
         """
@@ -221,6 +236,16 @@ class MetaCIFAR100(CIFAR100):
             3. Reshapes data and splits into batches
             4. Returns support and query examples and labels
         """
+        support_xs, support_ys, query_xs, query_ys = sample_classes_and_split_data(self, item)
+        support_xs, support_ys, query_xs, query_ys = np.array(support_xs), np.array(support_ys), np.array(
+            query_xs), np.array(query_ys)
+        support_xs, query_xs, support_ys, query_ys = reshape_and_split_data(query_xs, query_ys, support_xs, self, support_ys)
+    
+        support_xs, query_xs = apply_transformations(self, support_xs, query_xs)
+    
+        return support_xs, support_ys, query_xs, query_ys
+    
+    def sample_classes_and_split_data(self, item):
         if self.fix_seed:
             np.random.seed(item)
         cls_sampled = np.random.choice(self.classes, self.n_ways, False)
@@ -237,12 +262,13 @@ class MetaCIFAR100(CIFAR100):
             query_xs_ids = np.random.choice(query_xs_ids, self.n_queries, False)
             query_xs.append(imgs[query_xs_ids])
             query_ys.append([idx] * query_xs_ids.shape[0])
-        support_xs, support_ys, query_xs, query_ys = np.array(support_xs), np.array(support_ys), np.array(
-            query_xs), np.array(query_ys)
+        return support_xs, support_ys, query_xs, query_ys
+    
+    def reshape_and_split_data(query_xs, query_ys, support_xs, self, support_ys):
         num_ways, n_queries_per_way, height, width, channel = query_xs.shape
         query_xs = query_xs.reshape((num_ways * n_queries_per_way, height, width, channel))
         query_ys = query_ys.reshape((num_ways * n_queries_per_way,))
-
+    
         support_xs = support_xs.reshape((-1, height, width, channel))
         if self.n_aug_support_samples > 1:
             support_xs = np.tile(support_xs, (self.n_aug_support_samples, 1, 1, 1))
@@ -250,11 +276,12 @@ class MetaCIFAR100(CIFAR100):
         support_xs = np.split(support_xs, support_xs.shape[0], axis=0)
         query_xs = query_xs.reshape((-1, height, width, channel))
         query_xs = np.split(query_xs, query_xs.shape[0], axis=0)
-
+        return support_xs, query_xs, support_ys, query_ys
+    def apply_transformations(self, support_xs, query_xs):
         support_xs = torch.stack([self.train_transform(x.squeeze()) for x in support_xs])
         query_xs = torch.stack([self.test_transform(x.squeeze()) for x in query_xs])
-
-        return support_xs, support_ys, query_xs, query_ys
+        return support_xs, query_xs
+    
 
     def __len__(self):
         """
