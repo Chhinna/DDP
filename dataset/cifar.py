8cf17df131a6180882012d7ac00325697db91454
CodeAnt AI <chinmay@codeant.ai>
2023-12-17 16:04:58 +0000

    Changed files

--- dataset/cifar.py
+++ dataset/cifar.py
@@ -13,34 +13,51 @@ from torch.utils.data import Dataset
 class CIFAR100(Dataset):
     """support FC100 and CIFAR-FS"""
     def __init__(self, args, partition='train', pretrain=True, is_sample=False, k=4096,
-                 transform=None):
-        """
-        Initializes a dataset
-        Args: 
-            self: The object
-            args: Arguments
-            partition: Dataset partition ('train' or 'test')
-            pretrain: Whether to pretrain
-            is_sample: Whether to sample
-            k: Number of negative samples
-            transform: Image transformations
-        Returns:
-            None
-        Processes data:
-            - Loads data from file
-            - Normalizes images
-            - Preprocesses labels
-            - Samples positive and negative pairs if is_sample is True
-        """
-        super(Dataset, self).__init__()
-        self.data_root = args.data_root
-        self.partition = partition
-        self.data_aug = args.data_aug
-        self.mean = [0.5071, 0.4867, 0.4408]
-        self.std = [0.2675, 0.2565, 0.2761]
-        self.normalize = transforms.Normalize(mean=self.mean, std=self.std)
-        self.pretrain = pretrain
-
+                     transform=None):
+            """
+            Initializes a dataset
+            Args: 
+                self: The object
+                args: Arguments
+                partition: Dataset partition ('train' or 'test')
+                pretrain: Whether to pretrain
+                is_sample: Whether to sample
+                k: Number of negative samples
+                transform: Image transformations
+            Returns:
+                None
+            Processes data:
+                - Loads data from file
+                - Normalizes images
+                - Preprocesses labels
+                - Samples positive and negative pairs if is_sample is True
+            """
+            super(Dataset, self).__init__()
+            self.data_root = args.data_root
+            self.partition = partition
+            self.data_aug = args.data_aug
+            self.mean = [0.5071, 0.4867, 0.4408]
+            self.std = [0.2675, 0.2565, 0.2761]
+            self.normalize = transforms.Normalize(mean=self.mean, std=self.std)
+            self.pretrain = pretrain
+    
+            set_image_transformations(transform, self)
+    
+    
+            self.file_pattern = '%s.pickle'
+            if not self.pretrain:
+                pass
+    
+            self.data = {}
+    
+            load_data_from_pickle_file(self, partition)
+    
+            # pre-process for contrastive sampling
+            self.k = k
+            self.is_sample = is_sample
+            preprocess_for_contrastive_sampling(self)
+    
+    def set_image_transformations(transform, self):
         if transform is None:
             if self.partition == 'train' and self.data_aug:
                 self.transform = transforms.Compose([
@@ -60,14 +77,8 @@ class CIFAR100(Dataset):
                 ])
         else:
             self.transform = transform
-
-
-        self.file_pattern = '%s.pickle'
-        if not self.pretrain:
-            pass
-
-        self.data = {}
-
+    
+    def load_data_from_pickle_file(self, partition):
         with open(os.path.join(self.data_root, self.file_pattern % partition), 'rb') as f:
             data = pickle.load(f, encoding='latin1')
             self.imgs = data['data']
@@ -81,30 +92,28 @@ class CIFAR100(Dataset):
                     cur_class += 1
             new_labels = [label2label[label] for (idx, label) in enumerate(labels)]
             self.labels = new_labels
-
-        # pre-process for contrastive sampling
-        self.k = k
-        self.is_sample = is_sample
+    def preprocess_for_contrastive_sampling(self):
         if self.is_sample:
             self.labels = np.asarray(self.labels)
             self.labels = self.labels - np.min(self.labels)
             num_classes = np.max(self.labels) + 1
-
+    
             self.cls_positive = [[] for _ in range(num_classes)]
             for i in range(len(self.imgs)):
                 self.cls_positive[self.labels[i]].append(i)
-
+    
             self.cls_negative = [[] for _ in range(num_classes)]
             for i in range(num_classes):
                 for j in range(num_classes):
                     if j == i:
                         continue
                     self.cls_negative[i].extend(self.cls_positive[j])
-
+    
             self.cls_positive = [np.asarray(self.cls_positive[i]) for i in range(num_classes)]
             self.cls_negative = [np.asarray(self.cls_negative[i]) for i in range(num_classes)]
             self.cls_positive = np.asarray(self.cls_positive)
             self.cls_negative = np.asarray(self.cls_negative)
+    
 
     def __getitem__(self, item):
         """
@@ -150,31 +159,38 @@ class CIFAR100(Dataset):
 class MetaCIFAR100(CIFAR100):
 
     def __init__(self, args, partition='train', train_transform=None, test_transform=None, fix_seed=True):
-        """
-        Initialize MetaCIFAR100 dataset
-        Args:
-            args: Arguments - Contains dataset configuration
-            partition: Dataset partition ('train' or 'test') 
-            train_transform: Transformations for support set
-            test_transform: Transformations for query set
-            fix_seed: Fix random seed
-        Returns: 
-            self: MetaCIFAR100 object
-        Processing Logic:
-            - Initialize base class
-            - Set dataset hyperparameters
-            - Define transformations
-            - Split data into classes
-            - Shuffle class data
-        """
-        super(MetaCIFAR100, self).__init__(args, partition, False)
-        self.fix_seed = fix_seed
-        self.n_ways = args.n_ways
-        self.n_shots = args.n_shots
-        self.n_queries = args.n_queries
-        self.classes = list(self.data.keys())
-        self.n_test_runs = args.n_test_runs
-        self.n_aug_support_samples = args.n_aug_support_samples
+            """
+            Initialize MetaCIFAR100 dataset
+            Args:
+                args: Arguments - Contains dataset configuration
+                partition: Dataset partition ('train' or 'test') 
+                train_transform: Transformations for support set
+                test_transform: Transformations for query set
+                fix_seed: Fix random seed
+            Returns: 
+                self: MetaCIFAR100 object
+            Processing Logic:
+                - Initialize base class
+                - Set dataset hyperparameters
+                - Define transformations
+                - Split data into classes
+                - Shuffle class data
+            """
+            super(MetaCIFAR100, self).__init__(args, partition, False)
+            self.fix_seed = fix_seed
+            self.n_ways = args.n_ways
+            self.n_shots = args.n_shots
+            self.n_queries = args.n_queries
+            self.classes = list(self.data.keys())
+            self.n_test_runs = args.n_test_runs
+            self.n_aug_support_samples = args.n_aug_support_samples
+            set_train_transformations(train_transform, self)
+    
+            set_test_transformations(test_transform, self)
+    
+            populate_data_dict(self)
+    
+    def set_train_transformations(train_transform, self):
         if train_transform is None:
             self.train_transform = transforms.Compose([
                 lambda x: Image.fromarray(x),
@@ -187,7 +203,8 @@ class MetaCIFAR100(CIFAR100):
             ])
         else:
             self.train_transform = train_transform
-
+    
+    def set_test_transformations(test_transform, self):
         if test_transform is None:
             self.test_transform = transforms.Compose([
                 lambda x: Image.fromarray(x),
@@ -196,31 +213,40 @@ class MetaCIFAR100(CIFAR100):
             ])
         else:
             self.test_transform = test_transform
-
+    def populate_data_dict(self):
         self.data = {}
         for idx in range(self.imgs.shape[0]):
             if self.labels[idx] not in self.data:
                 self.data[self.labels[idx]] = []
             self.data[self.labels[idx]].append(self.imgs[idx])
         self.classes = list(self.data.keys())
+    
 
     def __getitem__(self, item):
-        """
-        Samples data for few-shot classification tasks
-        Args: 
-            self: The dataset object
-            item: The index of the task
-        Returns:
-            support_xs: Support set images
-            support_ys: Support set labels 
-            query_xs: Query set images
-            query_ys: Query set labels
-        Processing Logic:
-            1. Samples classes and splits data into support and query sets
-            2. Applies data augmentation and transformations
-            3. Reshapes data and splits into batches
-            4. Returns support and query examples and labels
-        """
+            """
+            Samples data for few-shot classification tasks
+            Args: 
+                self: The dataset object
+                item: The index of the task
+            Returns:
+                support_xs: Support set images
+                support_ys: Support set labels 
+                query_xs: Query set images
+                query_ys: Query set labels
+            Processing Logic:
+                1. Samples classes and splits data into support and query sets
+                2. Applies data augmentation and transformations
+                3. Reshapes data and splits into batches
+                4. Returns support and query examples and labels
+            """
+            query_xs, query_ys, support_xs, support_ys = sample_support_query_sets(self, item)
+            support_xs, query_xs, support_ys, query_ys = reshape_and_split_batches(query_xs, query_ys, support_xs, self, support_ys)
+    
+            support_xs, query_xs = apply_transformations(self, support_xs, query_xs)
+    
+            return support_xs, support_ys, query_xs, query_ys
+    
+    def sample_support_query_sets(self, item):
         if self.fix_seed:
             np.random.seed(item)
         cls_sampled = np.random.choice(self.classes, self.n_ways, False)
@@ -239,10 +265,13 @@ class MetaCIFAR100(CIFAR100):
             query_ys.append([idx] * query_xs_ids.shape[0])
         support_xs, support_ys, query_xs, query_ys = np.array(support_xs), np.array(support_ys), np.array(
             query_xs), np.array(query_ys)
+        return query_xs, query_ys, support_xs, support_ys
+    
+    def reshape_and_split_batches(query_xs, query_ys, support_xs, self, support_ys):
         num_ways, n_queries_per_way, height, width, channel = query_xs.shape
         query_xs = query_xs.reshape((num_ways * n_queries_per_way, height, width, channel))
         query_ys = query_ys.reshape((num_ways * n_queries_per_way,))
-
+    
         support_xs = support_xs.reshape((-1, height, width, channel))
         if self.n_aug_support_samples > 1:
             support_xs = np.tile(support_xs, (self.n_aug_support_samples, 1, 1, 1))
@@ -250,11 +279,12 @@ class MetaCIFAR100(CIFAR100):
         support_xs = np.split(support_xs, support_xs.shape[0], axis=0)
         query_xs = query_xs.reshape((-1, height, width, channel))
         query_xs = np.split(query_xs, query_xs.shape[0], axis=0)
-
+        return support_xs, query_xs, support_ys, query_ys
+    def apply_transformations(self, support_xs, query_xs):
         support_xs = torch.stack([self.train_transform(x.squeeze()) for x in support_xs])
         query_xs = torch.stack([self.test_transform(x.squeeze()) for x in query_xs])
-
-        return support_xs, support_ys, query_xs, query_ys
+        return support_xs, query_xs
+    
 
     def __len__(self):
         """
